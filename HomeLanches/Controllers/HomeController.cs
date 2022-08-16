91cab2a3b0fbde1f08b86e4e368522fe08bcd5d9
Marcos Nolasco <manolasco@gmail.com>
2022-08-15 21:19:42 -0300

    Ajuste no HomeController e exclusão da página Privacy

--- HomeLanches/Controllers/HomeController.cs
+++ HomeLanches/Controllers/HomeController.cs
@@ -5,20 +5,8 @@ using System.Diagnostics;
 namespace HomeLanches.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+    {        
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
