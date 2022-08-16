9a3d0c38a979b22bcff7e4693b3a4278e19586bd
Marcos Nolasco <manolasco@gmail.com>
2022-08-16 20:53:07 -0300

    Criação das classes de domínio Categoria e Lanche

---
+++ HomeLanches/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeLanches.Models
+{
+    public class Categoria
+    {
+        public int CategriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
